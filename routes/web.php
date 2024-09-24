3caedc890a7b50d333642ad54154d26609d22a22
Safar Umarov <safarumarov711@gmail.com>
2024-09-25 03:00:36 +0700

    added swagger via npm

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/api-docs/swagger', function () {
+    return view('api-docs.swagger');
+})->name('api-docs');
--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,15 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/swagger.js'],
             refresh: true,
         }),
     ],
+    // Смотри: https://stackoverflow.com/questions/78029463/vite-config-on-docker
+    server: {
+        host: '0.0.0.0',
+        hmr: {
+            host: 'localhost'
+        }
+    }
 });
